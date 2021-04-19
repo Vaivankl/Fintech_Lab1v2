e12eee5da7e5ef8b3c5bc584405257a482c687a4
Vaivankl <vaiva.niuklyte@knf.stud.vu.lt>
2021-04-19 19:07:54 +0300

    Changing text colour

--- app/src/main/java/com/example/fintech_lab1/MainActivity.java
+++ app/src/main/java/com/example/fintech_lab1/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.fintech_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvmain;
+    Button button2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvmain = findViewById(R.id.tvmain);
+        this. button2 = findViewById(R.id. button2);
+
     }
 
     public void OnBtnClick(View view) {
-        this.tvmain.setText("Text was changed !");
+        this.tvmain.setText(R.string.Changing_text_1);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvmain.setTextColor(Color.RED);
+            }
+        });
     }
 }
 

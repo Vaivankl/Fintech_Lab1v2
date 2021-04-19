f53e193bb1eb703fcfe73c4e9e87b3223cdc0335
Vaivankl <vaiva.niuklyte@knf.stud.vu.lt>
2021-04-19 18:30:13 +0300

    2nd commmit

--- app/src/main/java/com/example/fintech_lab1/MainActivity.java
+++ app/src/main/java/com/example/fintech_lab1/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.fintech_lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvmain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvmain = findViewById(R.id.tvmain);
+    }
+
+    public void OnBtnClick(View view) {
+        this.tvmain.setText("Text was changed !");
     }
 }
+
